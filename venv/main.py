d262598dfbc38235bf86fdb03d2e7102fb069e2b
Themockingjester <yashmathur123123@gmail.com>
2021-01-25 15:05:58 +0530

    updated ui

--- venv/main.py
+++ venv/main.py
@@ -1,7 +1,7 @@
 from kivy.core.text import LabelBase
 from kivy.config import Config
 
-
+from kivy.uix.behaviors import DragBehavior
 from kivymd.app import MDApp
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.properties import ListProperty, ObjectProperty
@@ -10,9 +10,13 @@ from kivymd.uix.menu import MDDropdownMenu
 from kivy.uix.image import Image
 from kivymd.uix.behaviors import TouchBehavior
 
+
+
 class ImageButtonWithDoubleTouch(Image,TouchBehavior):
     def on_double_tap(self,instance,*args):
-        print("hi")
+        print(self.source)
+class DraggableImageButtonWithDoubleTouch(DragBehavior, ImageButtonWithDoubleTouch):
+    pass
 class PencilSizeChanger(BoxLayout):
     pass
 class BuilderWindow(MDBoxLayout):
@@ -22,7 +26,7 @@ class BuilderWindow(MDBoxLayout):
 class uiApp(MDApp):
 
     def selected(self):
-        print("gfhgf")
+        print(self)
     def build(self):
         self.screen_manager = ScreenManager()
 
@@ -40,11 +44,13 @@ class uiApp(MDApp):
             width_mult=3,
         )
         self.menu.bind(on_release=self.menu_callback)
-        wimg = ImageButtonWithDoubleTouch(source='images/sensors/arduino.png',width=400,height=400)
-        self.buildersccreen.container.add_widget(wimg)
-        wimk = Image(source='images/sensors/passive_buzzer.png',allow_stretch=False,keep_ratio=False,width=200,height=200)
-        self.buildersccreen.container.add_widget(wimk)
+
+
         return self.screen_manager
+    def image_adder(self):
+        wimg = DraggableImageButtonWithDoubleTouch(source='images/sensors/arduino.png', width=400,size_hint_y= None,size_hint_x= None, height=400)
+        self.buildersccreen.container.add_widget(wimg)
+
     def menu_callback(self, instance_menu, instance_menu_item):
         print("hilk")
         print(instance_menu, instance_menu_item)
@@ -53,4 +59,3 @@ class uiApp(MDApp):
 
 LabelBase.register(name='pacifico',fn_regular='fonts/Pacifico/Pacifico-Regular.ttf')
 uiApp().run()
-
