f024e622a2da02993c8a65f871496a527f02fb1d
Themockingjester <yashmathur123123@gmail.com>
2021-01-26 15:29:33 +0530

    canvas updated

--- venv/main.py
+++ venv/main.py
@@ -1,32 +1,39 @@
 from kivy.core.text import LabelBase
 from kivy.config import Config
-
+import time
 from kivy.uix.behaviors import DragBehavior
 from kivymd.app import MDApp
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.properties import ListProperty, ObjectProperty
-from kivymd.uix.boxlayout import MDBoxLayout,BoxLayout
+from kivymd.uix.boxlayout import MDBoxLayout, BoxLayout
 from kivymd.uix.menu import MDDropdownMenu
 from kivy.uix.image import Image
 from kivymd.uix.behaviors import TouchBehavior
 
 
+class ImageButtonWithDoubleTouch(Image, TouchBehavior):
+    def on_double_tap(self, instance, *args):
+        pass
 
-class ImageButtonWithDoubleTouch(Image,TouchBehavior):
-    def on_double_tap(self,instance,*args):
-        print(self.source)
+
 class DraggableImageButtonWithDoubleTouch(DragBehavior, ImageButtonWithDoubleTouch):
     pass
+
+
 class PencilSizeChanger(BoxLayout):
     pass
+
+
 class BuilderWindow(MDBoxLayout):
     button = ObjectProperty(None)
     container = ObjectProperty(None)
+
 
 class uiApp(MDApp):
 
     def selected(self):
         print(self)
+
     def build(self):
         self.screen_manager = ScreenManager()
 
@@ -45,17 +52,27 @@ class uiApp(MDApp):
         )
         self.menu.bind(on_release=self.menu_callback)
 
-
         return self.screen_manager
+
     def image_adder(self):
-        wimg = DraggableImageButtonWithDoubleTouch(source='images/sensors/arduino.png', width=400,size_hint_y= None,size_hint_x= None, height=400)
+        # self.image_pos_holder = {}
+        for i in self.buildersccreen.container.children:
+            print(i)
+
+        wimg = DraggableImageButtonWithDoubleTouch(source='images/sensors/arduino.png', width=400, size_hint_y=None,
+                                                   size_hint_x=None, height=400)
         self.buildersccreen.container.add_widget(wimg)
+        # for i in self.buildersccreen.container.children:
+        #     try:
+        #         i.pos = (self.image_pos_holder.get(i)[0],self.image_pos_holder.get(i)[1])
+        #         print(i.pos)
+        #     except:
+        #         print("err")
 
     def menu_callback(self, instance_menu, instance_menu_item):
         print("hilk")
         print(instance_menu, instance_menu_item)
 
 
-
-LabelBase.register(name='pacifico',fn_regular='fonts/Pacifico/Pacifico-Regular.ttf')
+LabelBase.register(name='pacifico', fn_regular='fonts/Pacifico/Pacifico-Regular.ttf')
 uiApp().run()
